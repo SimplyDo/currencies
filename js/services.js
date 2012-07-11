3620e7a335c762556c341bd6bfa8335959af9255
Alexander Kohlhofer <alexander@kohlhofer.com>
2012-07-11 13:11:54 -0700

    added api key from open exchange rates

--- js/services.js
+++ js/services.js
@@ -9,7 +9,7 @@ var servicesModule = angular.module('myApp.services', ['ngResource']);
 
 
 servicesModule.factory('openExchangeRates', function($resource){
-  return $resource('http://openexchangerates.org/:path', {callback:'JSON_CALLBACK'}, {
+  return $resource('http://openexchangerates.org/:path', {callback:'JSON_CALLBACK',app_id:'5c2dba0f887544a4b196eeaa8a3052f4'}, {
     getRates: {method:'JSONP', params:{path:'latest.json'}},
     getLegend: {method:'JSONP', params:{path:'currencies.json'}}
   });
